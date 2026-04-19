3d7f03e4cbe7e91fee490c3a6f3e1c709a30f743
Mariano Losada <89931647+marianodip12@users.noreply.github.com>
2026-04-18 21:44:35 -0300

    Delete handball-stats directory

--- handball-stats/src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const url = 'https://fpgojijkzkkhlfqfardn.supabase.co'
-const key = 'sb_publishable_Mpf4slzcVyGWkQqNOeugFQ_a79k2iq5'
-
-export const supabase = createClient(url, key)
